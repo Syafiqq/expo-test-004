452ff1d14157759ce08e8d151f8656c5dbc51902
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-19 20:18:29 +0700

    W1.2: 002, lib, setup mmkv

--- src/di/data/datasource/cache-provider.ts
+++ src/di/data/datasource/cache-provider.ts
@@ -1,7 +1,10 @@
 import {Container} from "inversify";
 import {TypesMap} from "@/di/container.type";
+import {MMKV} from "react-native-mmkv";
+import {storage} from "@/data/datasource/cache/impl/mmkv";
 
 const configure = (container: Container, types: TypesMap): void => {
+  container.bind<MMKV>(types.mmkv).toConstantValue(storage);
 }
 
 export default configure;
